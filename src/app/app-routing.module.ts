d7e4d758f71b38ee1405cc840bae182e81939c2e
mohsenshahbaze@gmail.com <mohsenshahbaze@gmail.com>
2023-12-24 02:50:32 +0330

    add some components and module and router to project create simple header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFountComponent } from './not-fount/not-fount.component';
-import {AboutComponent} from "./about/about.component";
-import {ContactComponent} from "./contact/contact.component";
-import {UnitsModule} from "./units/units.module";
+import {AboutComponent} from "./pages/about/about.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {UnitsModule} from "./pages/units/units.module";
 
 const routes: Routes = [{
   path: '',
@@ -18,7 +18,7 @@ const routes: Routes = [{
     },
     {
       path: 'home',
-      loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
     },
     {
       path: 'about',
@@ -30,11 +30,11 @@ const routes: Routes = [{
     },
     {
       path: 'monitoring',
-      loadChildren: () => import('./monitoring/monitoring.module').then(m => m.MonitoringModule)
+      loadChildren: () => import('./pages/monitoring/monitoring.module').then(m => m.MonitoringModule)
     },
     {
       path: 'unit-list',
-      loadChildren: () => import('./units/units.module').then(m => m.UnitsModule)
+      loadChildren: () => import('./pages/units/units.module').then(m => m.UnitsModule)
     },
     { path: '404', component: NotFountComponent },
     { path: '**', redirectTo: '' }
